2efb8f1639a262f537a5347f5635662ea24c4f32
Kevin Salins <kevinsalinskevin@gmail.com>
2026-06-14 15:41:08 +0530

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.cpp
@@ -0,0 +1,35 @@
+class LRUCache {
+private:
+    unordered_map<int, pair<int, list<int>::iterator>> cache;
+    list<int> order;
+    int capacity;
+public:
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+    }
+    
+    int get(int key) {
+        if(cache.find(key) == cache.end()) return -1;
+
+        auto entry = cache[key];
+        order.erase(entry.second);
+        order.push_back(key);
+        cache[key].second = --order.end();
+        return cache[key].first;
+    }
+    
+    void put(int key, int value) {
+        if(cache.find(key) != cache.end())
+        {
+            order.erase(cache[key].second);
+        } else if(capacity == cache.size())
+        {
+            int l = order.front();
+            order.pop_front();
+            cache.erase(l);
+        }
+        order.push_back(key);
+        cache[key] = {value, --order.end()};
+        
+    }
+};
