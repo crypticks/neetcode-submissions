ee344a0cc5d7be9e09e7bbe49fa717b828dd1af7
Kevin Salins <kevinsalinskevin@gmail.com>
2026-07-01 21:25:21 +0530

    Add: missing-number - submission-1

---
+++ Data Structures & Algorithms/missing-number/submission-1.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int res = 0;
+        for(int i = 0; i <= nums.size(); i++) res ^= i;
+        for(int num : nums) res ^= num;
+        return res;
+    }
+};
