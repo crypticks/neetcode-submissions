e4c95af3e48ddec68f9c2715583f2a52c39aec72
Kevin Salins <kevinsalinskevin@gmail.com>
2026-07-02 12:25:04 +0530

    Add: append-characters-to-string-to-make-subsequence - submission-0

---
+++ Data Structures & Algorithms/append-characters-to-string-to-make-subsequence/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int appendCharacters(string s, string t) {
+        int i = 0, j = 0;
+        int m = s.size(), n = t.size();
+        while(i < m && j < n)
+        {
+            if(s[i] == t[j]) j++;
+            i++;
+        }
+        return n - j;
+    }
+};
